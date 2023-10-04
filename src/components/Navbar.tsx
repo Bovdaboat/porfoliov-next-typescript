7ecc0fa530d0f4ebfd66cf30f3b69933cd19623b
bovdaboat <ignatgabov@gmail.com>
2023-10-03 20:55:26 -0700

    finished doing the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,8 @@
 "use client"
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import {ModeToggle} from "@/components/Toggle-mode";
 
 const Navbar: React.FC = () => {
 
@@ -13,28 +14,29 @@ const Navbar: React.FC = () => {
 
     return (
         <nav className=" text-primary">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 border border-green-50 rounded">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 border border-green-50">
                 <div className=" flex items-center  justify-between h-16 font-mono">
                     <div className="flex items-center">
                     <div className="flex shrink-0">
                     <a className=" text-2xl" href="https://www.linkedin.com/in/ignat-gabov-97319a145/">BovDaBoat</a>
                     </div>
                     </div>
-                    <div className="hidden md:block">
+                    <div className="hidden md:block text-primary">
                     <div className="ml-4 flex items-center space-x-4 p-2">
-                        <a  href="/" className="text-white">
+                        <ModeToggle />
+                        <a  href="/" className="">
                             Home
                         </a>
-                        <a  href="/skills" className="text-white">
+                        <a  href="/skills" className="">
                             Skills
                         </a>
-                        <a  href="/projects" className="text-white">
+                        <a  href="/projects" className="">
                             Project's
                         </a>
-                        <a  href="/about" className="text-white">
+                        <a  href="/about" className="">
                             About
                         </a>
-                        <a  href="/contact" className="text-white">
+                        <a  href="/contact" className="">
                             Contact
                         </a>
                     </div>
@@ -68,18 +70,21 @@ const Navbar: React.FC = () => {
                         <a  href="/" className="text-white block p-2">
                             Home
                         </a>
-                        <a  href="/skills" className="text-white block p-2">
+                        <a  href="/skills" className=" block p-2">
                             Skills
                         </a>
-                        <a  href="/projects" className="text-white block p-2">
+                        <a  href="/projects" className=" block p-2">
                             Project's
                         </a>
-                        <a  href="/about" className="text-white block p-2">
+                        <a  href="/about" className=" block p-2">
                             About
                         </a>
-                        <a  href="/contact" className="text-white block p-2">
+                        <a  href="/contact" className=" block p-2">
                             Contact
                         </a>
+                        <div className="px-4 pt-2">
+                        <ModeToggle />
+                        </div>
                     </div>
                 </div>
             )}
