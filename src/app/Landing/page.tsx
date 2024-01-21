402f84403c2d5c991b74cf5013fe61c3b91d7257
bovdaboat <ignatgabov@gmail.com>
2024-01-20 21:59:11 -0800

    changing to freelnacer

--- src/app/Landing/page.tsx
+++ src/app/Landing/page.tsx
@@ -6,7 +6,12 @@ const Landing: React.FC = () => {
     return (
         <div className="min-h-screen flex items-center justify-center bg-base-200">
             <div className="text-center">
-                <h1 className="text-5xl text-primary font-bold mt-22 pt-20">Hello Friend&apos;s</h1>
+                <div className="text-4xl text-primary font-bold mt-22 pt-20 mx-7">
+                <h1 className="text-4xl text-primary font-bold mt-22 pt-20 mx-7">
+                    Looking for a website that speaks to your unique style?
+                    <span className=" p-3 text-blue-600">Let&apos;s set sail on this journey!</span>
+                </h1>
+                </div>
                 <Image
                     src="/Images/mainImg.png"
                     alt="boat Image"
@@ -14,8 +19,10 @@ const Landing: React.FC = () => {
                     width={500}
                     height={500}
                 />
-                <p className="text-primary mt-2 px-4 font-mono">
-                    I&apos;m Ignat, known as Bov in the tech world, and I excel in frontend development.
+                <p className="text-primary mt-2 px-10 font-mono">
+
+                    Hello, I&apos;m Ignat, aka Bov in tech. I specialize in frontend development, crafting seamless
+                    interfaces for web 2.0 and web 3.0. Let&apos;s enhance your digital presence together.
                 </p>
             </div>
         </div>
